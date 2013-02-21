11bb2960c8e755b41e9c817b1fad024566083bf8
Pietari Kettunen <kettunen@gmail.com>
2013-02-21 23:14:24 +0100

    refactored chatengine

--- chatengine.js
+++ chatengine.js
@@ -1,41 +1,48 @@
-var socketio = require('socket.io');
+var socketio = require('socket.io'),
+    usernames = {},
+    io;
 
-function configure(io) {
+exports.io = function(server) {
+  io = socketio.listen(server);
+
+  configure();
+  io.sockets.on('connection', function (socket) {
+
+    socket.on('sendchat', function (msg) {
+      io.sockets.emit('updatechat', socket.username, msg);
+    });
+
+    socket.on('joinchat', function (username) {
+      joinChat(socket, username);
+    });
+
+    socket.on('disconnect', function() {
+      disconnectUser(socket);
+    });
+  });
+};
+
+function configure() {
   io.configure(function () { 
     io.set("transports", ["xhr-polling"]); 
     io.set("polling duration", 10); 
   });
 }
 
-exports.io = function(server) {
-  var io = socketio.listen(server),
-      usernames = {};
-  configure(io);
+function joinChat(socket, username) {
+  if (socket.username != undefined) {
+    delete usernames[socket.username];
+    socket.emit('updatechat', 'SERVER', socket.username + ' changed name to ' +  username);
+  } else {
+    socket.emit('updatechat', 'SERVER', username + ' connected');
+  }
+  socket.username = username;
+  usernames[username] = username;
+  io.sockets.emit('updateusers', usernames);
+}
 
-  io.sockets.on('connection', function (socket) {
-    console.log('Connected');
-
-    socket.on('sendchat', function (data) {
-      io.sockets.emit('updatechat', socket.username, data);
-    });
-
-    socket.on('joinchat', function (username) {
-      console.log('Joining chat: ' + socket.username);
-      if (socket.username != undefined) {
-        delete usernames[socket.username];
-        socket.emit('updatechat', 'SERVER', socket.username + ' changed name to ' +  username);
-      } else {
-        socket.emit('updatechat', 'SERVER', username + ' connected');
-      }
-      socket.username = username;
-      usernames[username] = username;
-      io.sockets.emit('updateusers', usernames);
-    });
-
-    socket.on('disconnect', function() {
-      delete usernames[socket.username];
-      io.sockets.emit('updateusers', usernames);
-      socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has left the building');
-    });
-  });
-};
+function disconnectUser(socket) {
+  delete usernames[socket.username];
+  io.sockets.emit('updateusers', usernames);
+  socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has left the building');
+}
